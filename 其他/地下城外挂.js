f879315bf1e57c47b4ae4b6058eb5727c456e939
Alan Best <47408535+alan-best@users.noreply.github.com>
2021-05-03 17:35:12 +0800

    Update 地下城外挂.js

--- 其他/地下城外挂.js
+++ 其他/地下城外挂.js
@@ -1,4 +1,4 @@
-// noinspection SpellCheckingInspection
+// noinspection SpellCheckingInspection,NonAsciiCharacters
 
 
 /**
@@ -13,39 +13,67 @@
  *
  *
  */
-console.log("地下城外挂已开启");
-console.info("Script by Alan_Best","text-shadow:2px 2px 0 #eee;color:#139eff");
+if(window.DUNGEON_HELPER_INSTALLED) {throw new Error("不能重复执行脚本")}
+window.DUNGEON_HELPER_INSTALLED=true;
+window.autoSelectNum=0
+window.selectNum=0;
+window.problemsStat={}
+console.clear();
+console.group("信息");
+console.log("%c地下城外挂已开启","color: red;font-size:20px;text-shadow:0 0 3px");
+console.log("%cScript by Alan_Best","text-shadow:2px 2px 0 #eee;color:#139eff");
+console.groupEnd();
+console.group("帮助");
+console.log("%c进行战斗答题时, 此脚本将会生效","text-shadow:2px 2px 0 #eee;color:#139eff");
+console.log("%c如果遇到题库中没有的题目, 则会在控制台出现选项, 你需要在控制台选择正确答案(或者选择skip跳过), 然后就会记录进题库","text-shadow:2px 2px 0 #eee;color:#139eff");
+console.log("%c如果出现错乱, 请刷新页面","text-shadow:2px 2px 0 #eee;color:#fc8308");
+console.groupEnd();
+console.groupCollapsed("题库操作方法");
+console.log("%c使用 mergeProblemsLib(题库数据) 可以更新你的题库","color:#1e9eff");
+console.log("%c使用 exportProblemsLib() 可以分享题库","color:#1e9eff");
+console.log("%c使用 clearProblemsLib() 删除题库","color:#1e9eff");
+console.groupEnd();
+console.group("其他");
+console.log("%c输入 stat 获取统计(只针对本次脚本运行)","color:#1e9eff");
+console.groupEnd();
 function main() {
   let dialog = document.querySelector("._3jDd_WxlRxgzIRrg58zLFo");
-  if (!dialog) return setTimeout(main, 100);
+  if (!dialog) return setTimeout(main, 10);
   let dialogContent = dialog.querySelector("div").innerText.split("\n")[0];
   let dialogSelects = dialog.querySelectorAll(".GjXxM1DCq_ffiC7NBnfMa._2URGKFZ7--uVo_vDpFNGxk._2x2k1tEiuyNeNRlaHFnMnO._2URGKFZ7--uVo_vDpFNGxk")
   if (!dialogSelects || dialogSelects.length === 0) return setTimeout(main, 100);
+  window.problemsStat[dialogContent]?window.problemsStat[dialogContent]++:window.problemsStat[dialogContent]=1;
   let autoSelect = false
   if (localStorage.getItem("t_" + dialogContent)) {
     dialogSelects.forEach(i => {
       if (i.innerText === localStorage.getItem("t_" + dialogContent)) {
         i.click();
-        autoSelect = true
+        autoSelect = true;
+        console.groupCollapsed("执行自动选择");
+        console.log("题目: %c"+dialogContent,"color:#eaeaea");
+        console.log("自动选择答案: %c"+i.innerText,"color:#aaa");
+        console.groupEnd()
       }
     })
   }
   if (autoSelect) {
-    return setTimeout(main, 500)
+    window.autoSelectNum++
+    return setTimeout(main, 1)
   }
-  console.clear();
-  console.group("选择")
-  console.info(dialogContent);
+  console.group("手动选择")
+  console.info(`%c${dialogContent}`,"color:green");
   console.log(`a: ${dialogSelects[0].innerText}`);
   dialogSelects[1] && console.log(`b: ${dialogSelects[1].innerText}`);
   dialogSelects[2] && console.log(`c: ${dialogSelects[2].innerText}`)
-  console.info("使用a/b/c/skip来选择");
+  console.info("%c使用a/b/c/skip来选择","color:#1e9eff");
   console.groupEnd();
+
 
   function s(v) {
     localStorage.setItem("t_" + dialogContent, dialogSelects[{a: 0, b: 1, c: 2}[v]].innerText);
-    console.info("已选择");
-    setTimeout(main, 10);
+    console.info("%c已记录到题库","color:#34A853;");
+    window.selectNum++
+    setTimeout(main, 1);
   }
 
   Object.defineProperty(window, "a", {
@@ -71,5 +99,49 @@ function main() {
     },
     configurable: true
   });
+
 }
-main()
+main();
+Object.defineProperty(window,"mergeProblemsLib",{
+  configurable:true,
+  value:(json)=>{
+    try{
+    let oldLength = localStorage.length
+    Object.assign(localStorage,JSON.parse(json));
+    console.log(`%c题库新增${localStorage.length-oldLength}条数据`,"color:#34A853")}catch (e) {
+     console.error("合并题库时出错: "+e)
+
+    }
+  }
+})
+Object.defineProperty(window,"exportProblemsLib",{
+  configurable:true,
+  value:()=>{
+    console.group("题库导出数据");
+    console.log("`"+JSON.stringify(localStorage).replace(/\\"/g,`\\\\"`)+"`");
+    console.log("复制上方数据(或者点击COPY)")
+    console.groupEnd()
+  }
+});
+Object.defineProperty(window,"clearProblemsLib",{
+  configurable:true,
+  value:(b)=>{
+    if(!b){console.warn("此操作会完全清空你的所有题库, 请谨慎考虑. 如果确实要执行, 请使用 clearProblemsLib(true)")}else{
+      localStorage.clear();
+      console.warn("已清空题库")
+    }
+  }
+})
+Object.defineProperty(window, "stat", {
+  get() {
+    console.group("统计信息")
+    console.group("答题方式")
+    console.table({"手动":window.selectNum,"自动":window.autoSelectNum,"自动化比率":window.autoSelectNum/(window.selectNum+window.autoSelectNum)})
+    console.groupEnd();
+    console.groupCollapsed("题目出现次数");
+    console.table(window.problemsStat);
+    console.groupEnd()
+    console.groupEnd();
+  },
+  configurable: true
+});void 0;
